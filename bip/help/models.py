48862cb13c877e4c39a742307a3ade8180efe7f1
anna-kuleeva <aveelukann@gmail.com>
2023-03-30 01:43:54 +0300

    Добавила бд для своей части, но страница объявления пока не очень работает. Надо исправить. Немного дописала отчет.

--- bip/help/models.py
+++ bip/help/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Help(models.Model):
+    title = models.CharField('Краткое описание', max_length=255)
+    full_info = models.TextField('Подробное описание')
+    org_info = models.TextField('Описание организации')
+
+    def __str__(self):
+        return self.title
--- bip/main/models.py
+++ bip/main/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Desk(models.Model):
+    title = models.CharField('Краткое описание', max_length=255)
+
+    def __str__(self):
+        return self.title
