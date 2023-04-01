2354d98eeccb92e289f4e57772d645088163a3a7
anna-kuleeva <aveelukann@gmail.com>
2023-04-01 15:05:45 +0300

    Изменила работу бд, изменила модели и вью. Добавила html, но они не работают, нужно исправить.

--- bip/main/models.py
+++ bip/main/models.py
@@ -1,8 +1,11 @@
 from django.db import models
 
 # Create your models here.
-class Desk(models.Model):
+class Help(models.Model):
     title = models.CharField('Краткое описание', max_length=255)
+    full_info = models.TextField('Подробное описание')
+    org_info = models.TextField('Описание организации')
+    pubdate = models.DateTimeField('Дата публикации')
 
     def __str__(self):
         return self.title
--- bip/main/views.py
+++ bip/main/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+from .models import Help
 
-# Create your views here.
+# print list of all helps
 def index(request):
     # return HttpResponse("<h4>Главная сраница</h4>")
-    return render(request, "index.html")
+    list_of_helps = Help.objects.order_by('-pubdate')
+    return render(request, "index.html", {'list_of_helps': list_of_helps})
+
+def help(request, help_id):
+    try:
+        h = Help.objects.get(id=help_id)
+    except:
+        raise Http404("Страница не найдена!")
+
+    return render(request, "help.html", {'help': h})
