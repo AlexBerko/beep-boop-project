01aab160685e09f5da6dba553a5d60b7f0c74ae1
IlyaRid <redenkoid@gmail.com>
2023-08-05 16:33:32 +0300

    Залил venv

--- bipka/bipka/settings.py
+++ bipka/bipka/settings.py
@@ -42,11 +42,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.contrib.sites',
+    # 'django.contrib.sites',
 
     'allauth',
     'allauth.account',
-    'allauth.socialaccount',
+    # 'allauth.socialaccount',
 
     'drf_yasg',
 
